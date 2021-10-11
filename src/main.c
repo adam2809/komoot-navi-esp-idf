2bcf2b2d25f4f483610d274357a1b5c33faf7500
andek <adam.kule@gmail.com>
2021-10-11 20:01:49 +0200

    Makes basic functionality work

--- src/main.c
+++ src/main.c
@@ -131,6 +131,125 @@ void resolve_nav_data(uint8_t* data,struct nav_data_t* target){
     strcpy(target->street,(char*) &data[9]);
 }
 
+
+
+
+void write_number_icon(uint8_t dest[MAX_PAGE_COUNT*NUMBER_WIDTH],uint8_t icon[NUMBER_PAGE_COUNT*NUMBER_WIDTH],int display_pixel,int number_pixel){
+	int number_page = number_pixel/8;
+	int display_page = display_pixel/8;
+
+	// Write pixels from start pixel to the end of current page
+	if(display_pixel%8!=0){
+		int pixels_to_full_page_display = 8-(display_pixel%8);
+		int pixels_to_full_page_number = 8-(number_pixel%8);
+
+
+		for(int seg=0;seg<NUMBER_WIDTH;seg++){
+
+			uint8_t old_right = (dest[display_page*NUMBER_WIDTH+seg] << pixels_to_full_page_display) >> pixels_to_full_page_display;
+			uint8_t new_left;
+
+			if(pixels_to_full_page_number < pixels_to_full_page_display){
+				uint8_t curr_right = icon[number_page*NUMBER_WIDTH+seg] >> number_pixel%8;
+				uint8_t next_left = icon[(number_page+1)*NUMBER_WIDTH+seg] << pixels_to_full_page_number;
+
+				new_left = (next_left | curr_right)<< display_pixel%8;
+			}else{
+				new_left = (icon[number_page*NUMBER_WIDTH+seg] >> number_pixel%8) << (number_pixel%8+(pixels_to_full_page_number-pixels_to_full_page_display));
+			}
+
+			dest[display_page*NUMBER_WIDTH*seg] = new_left | old_right;
+		}
+
+		number_pixel+=pixels_to_full_page_display;
+		display_pixel+=pixels_to_full_page_display;
+		number_page = number_pixel/8;
+		display_page = display_pixel/8;
+	}
+
+	// Write full pages
+	while (number_pixel < NUMBER_PAGE_COUNT*8-8+1){
+		for(int seg=0;seg<NUMBER_WIDTH;seg++){
+			int pixels_to_full_page_number = 8-(number_pixel%8);
+
+			uint8_t new_display_page;
+			if(pixels_to_full_page_number < 8){
+				uint8_t curr_right = icon[number_page*NUMBER_WIDTH+seg] >> number_pixel%8;
+				uint8_t next_left = icon[(number_page+1)*NUMBER_WIDTH+seg] << pixels_to_full_page_number;
+
+				new_display_page = next_left | curr_right;
+			}else{
+				new_display_page = icon[number_page*NUMBER_WIDTH+seg];
+			}
+
+			dest[display_page*NUMBER_WIDTH+seg] = new_display_page;
+		}
+
+		number_pixel+=8;
+		display_pixel+=8;
+		number_page = number_pixel/8;
+		display_page = display_pixel/8;
+	}
+
+	// Write what is left of last page of number on the beginning of next display page
+	if (number_pixel < NUMBER_PAGE_COUNT*8){
+		for(int seg=0;seg<NUMBER_WIDTH;seg++){
+			int pixels_to_end = NUMBER_PAGE_COUNT*8 - number_pixel;
+			int pixels_from_last_page = 8 - pixels_to_end;
+
+			uint8_t old_left = (dest[display_page*NUMBER_WIDTH+seg] >> pixels_to_end) << pixels_to_end;
+			uint8_t new_right = icon[number_page*NUMBER_WIDTH+seg] >> pixels_from_last_page;
+
+			dest[display_page*NUMBER_WIDTH+seg] = old_left | new_right;
+		}
+	}
+}
+
+
+void display_numbers(uint8_t numbers[MAX_PAGE_COUNT*NUMBER_WIDTH]){
+	display_partial_image(&disp,numbers,0,MAX_PAGE_COUNT,METER_DISPLAY_SEG,NUMBER_WIDTH);
+}
+void display_nav_symbol(uint8_t symbol[MAX_PAGE_COUNT*MANEUVER_WIDTH]){
+    display_partial_image(&disp,symbol,0,8,0,64);
+}
+
+
+
+void display_meters(uint32_t meters){
+	char meters_str[10];
+	sprintf(meters_str,"%dm",meters);
+	ESP_LOGI(GATTC_TAG,"Displaying %d meters",meters);
+	uint8_t meters_display[MAX_PAGE_COUNT*NUMBER_WIDTH] = {0};
+
+	if(meters == 0){	
+		write_number_icon(meters_display,numbers[0],0,3);
+		display_numbers(meters_display);
+		return;
+	}
+
+	if(meters >= 1000){	
+		write_number_icon(meters_display,greater_than,42,3);
+		write_number_icon(meters_display,numbers[1],21,3);
+		write_number_icon(meters_display,km,0,3);
+		display_numbers(meters_display);
+		return;
+	}
+
+	
+	int curr_pixel = 0;
+	while(meters != 0){
+		uint32_t digit = meters%10;
+        ESP_LOGD(GATTC_TAG,"Writing num");
+
+		write_number_icon(meters_display,numbers[digit],curr_pixel,3);
+
+		meters/=10;
+		curr_pixel+=21;
+	}
+	display_numbers(meters_display);
+}
+
+
 void  read_nav_char_task(void *pvParameter){
     while(1){
         vTaskDelay(1000 / portTICK_PERIOD_MS);
@@ -142,6 +261,13 @@ void  read_nav_char_task(void *pvParameter){
                 ESP_LOGE(GATTC_TAG, "Error while reading characteristic");
             }
         }
+
+
+        if(curr_nav_data.direction!=0){
+            display_nav_symbol(nav_symbols[curr_nav_data.direction]);
+        }
+        
+        display_meters(curr_nav_data.distance);
     }
 }
 
@@ -533,128 +659,6 @@ void config_display(){
 	clear_display();
 }
 
-void write_number_icon(uint8_t dest[MAX_PAGE_COUNT*NUMBER_WIDTH],uint8_t icon[NUMBER_PAGE_COUNT*NUMBER_WIDTH],int display_pixel,int number_pixel){
-	int number_page = number_pixel/8;
-	int display_page = display_pixel/8;
-
-	// Write pixels from start pixel to the end of current page
-	if(display_pixel%8!=0){
-		int pixels_to_full_page_display = 8-(display_pixel%8);
-		int pixels_to_full_page_number = 8-(number_pixel%8);
-
-
-		for(int seg=0;seg<NUMBER_WIDTH;seg++){
-
-			uint8_t old_right = (dest[display_page*NUMBER_WIDTH+seg] << pixels_to_full_page_display) >> pixels_to_full_page_display;
-			uint8_t new_left;
-
-			if(pixels_to_full_page_number < pixels_to_full_page_display){
-				uint8_t curr_right = icon[number_page*NUMBER_WIDTH+seg] >> number_pixel%8;
-				uint8_t next_left = icon[(number_page+1)*NUMBER_WIDTH+seg] << pixels_to_full_page_number;
-
-				new_left = (next_left | curr_right)<< display_pixel%8;
-			}else{
-				new_left = (icon[number_page*NUMBER_WIDTH+seg] >> number_pixel%8) << (number_pixel%8+(pixels_to_full_page_number-pixels_to_full_page_display));
-			}
-
-			dest[display_page*NUMBER_WIDTH*seg] = new_left | old_right;
-		}
-
-		number_pixel+=pixels_to_full_page_display;
-		display_pixel+=pixels_to_full_page_display;
-		number_page = number_pixel/8;
-		display_page = display_pixel/8;
-	}
-
-	// Write full pages
-	while (number_pixel < NUMBER_PAGE_COUNT*8-8+1){
-		for(int seg=0;seg<NUMBER_WIDTH;seg++){
-			int pixels_to_full_page_number = 8-(number_pixel%8);
-
-			uint8_t new_display_page;
-			if(pixels_to_full_page_number < 8){
-				uint8_t curr_right = icon[number_page*NUMBER_WIDTH+seg] >> number_pixel%8;
-				uint8_t next_left = icon[(number_page+1)*NUMBER_WIDTH+seg] << pixels_to_full_page_number;
-
-				new_display_page = next_left | curr_right;
-			}else{
-				new_display_page = icon[number_page*NUMBER_WIDTH+seg];
-			}
-
-			dest[display_page*NUMBER_WIDTH+seg] = new_display_page;
-		}
-
-		number_pixel+=8;
-		display_pixel+=8;
-		number_page = number_pixel/8;
-		display_page = display_pixel/8;
-	}
-
-	// Write what is left of last page of number on the beginning of next display page
-	if (number_pixel < NUMBER_PAGE_COUNT*8){
-		for(int seg=0;seg<NUMBER_WIDTH;seg++){
-			int pixels_to_end = NUMBER_PAGE_COUNT*8 - number_pixel;
-			int pixels_from_last_page = 8 - pixels_to_end;
-
-			uint8_t old_left = (dest[display_page*NUMBER_WIDTH+seg] >> pixels_to_end) << pixels_to_end;
-			uint8_t new_right = icon[number_page*NUMBER_WIDTH+seg] >> pixels_from_last_page;
-
-			dest[display_page*NUMBER_WIDTH+seg] = old_left | new_right;
-		}
-	}
-}
-
-void display_numbers(uint8_t numbers[MAX_PAGE_COUNT*NUMBER_WIDTH]){
-	display_partial_image(&disp,numbers,0,MAX_PAGE_COUNT,METER_DISPLAY_SEG,NUMBER_WIDTH);
-}
-
-void display_meters(uint32_t meters){
-	uint8_t meters_display[MAX_PAGE_COUNT*NUMBER_WIDTH] = {0};
-
-	if(meters == 0){	
-		write_number_icon(meters_display,numbers[0],0,3);
-		display_numbers(meters_display);
-		return;
-	}
-
-	if(meters >= 1000){	
-		write_number_icon(meters_display,greater_than,42,3);
-		write_number_icon(meters_display,numbers[1],21,3);
-		write_number_icon(meters_display,km,0,3);
-		display_numbers(meters_display);
-		return;
-	}
-
-	
-	int curr_pixel = 0;
-	while(meters != 0){
-		uint32_t digit = meters%10;
-
-		write_number_icon(meters_display,numbers[digit],curr_pixel,3);
-
-		meters/=10;
-		curr_pixel+=21;
-	}
-	display_numbers(meters_display);
-
-	char meters_str[10];
-	sprintf(meters_str,"%dm",meters);
-	ESP_LOGI(GATTC_TAG,"Displaying %d meters",meters);
-}
-
-void dir_disp_task(void *pvParameter){
-	clear_display();
-    int curr_num=0;
-
-    while(1){
-        vTaskDelay(200 / portTICK_PERIOD_MS);
-
-        display_meters(curr_num);
-
-        curr_num+=10;
-    }
-}
-
 void app_main(void){
     // Initialize NVS.
     esp_err_t ret = nvs_flash_init();
@@ -715,5 +719,4 @@ void app_main(void){
     }
 
     config_display();
-	xTaskCreate(&dir_disp_task, "display_dir_on_oled", 4098, NULL, 5, NULL);
 }
