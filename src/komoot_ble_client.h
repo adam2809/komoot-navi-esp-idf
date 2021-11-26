e67169597df9e7b98212ca559bea1df4fbf8873e
andek <adam.kule@gmail.com>
2021-11-26 19:37:43 +0100

    Extracts ble client to separate file

---
+++ src/komoot_ble_client.h
@@ -0,0 +1,50 @@
+#include "esp_bt.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gattc_api.h"
+#include "esp_gatt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_gatt_common_api.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <string.h>
+
+#define PROFILE_NUM      1
+#define PROFILE_A_APP_ID 0
+#define INVALID_HANDLE   0
+#define SCAN_DURATION 30
+#define EXPECTED_ADV_DATA_LEN 21
+#define GATTC_TAG "GATTC_DEMO"
+
+
+struct gattc_profile_inst {
+    esp_gattc_cb_t gattc_cb;
+    uint16_t gattc_if;
+    uint16_t app_id;
+    uint16_t conn_id;
+    uint16_t service_start_handle;
+    uint16_t service_end_handle;
+    uint16_t char_handle;
+    esp_bd_addr_t remote_bda;
+};
+
+enum display_task_notify_value_t{
+    NOTIFY_VALUE_NAVIGATION,
+    NOTIFY_VALUE_PASSKEY
+};
+
+
+struct nav_data_t{
+    uint8_t direction;
+    uint32_t distance;
+    char street[100];
+};
+
+esp_err_t init_komoot_ble_client(uint32_t* _passkey_pointer,struct nav_data_t* _nav_data_pointer,TaskHandle_t* _display_nav_task_handle_pointer);
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+static void esp_gattc_cb(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param);
+static void gattc_profile_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param);
+void resolve_nav_data(uint8_t* data,struct nav_data_t* target);
+static char *esp_auth_req_to_str(esp_ble_auth_req_t auth_req);
+static const char *esp_key_type_to_str(esp_ble_key_type_t key_type);
+uint8_t* resolve_service_from_adv_data(uint8_t* adv_data,uint8_t adv_data_length);
