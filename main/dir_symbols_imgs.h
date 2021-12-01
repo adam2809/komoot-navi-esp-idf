75f07a415efe5d4d8cb56cc9f010217b2b947367
andek <adam.kule@gmail.com>
2021-12-01 02:40:10 +0100

    Adds dir symbols

---
+++ main/dir_symbols_imgs.h
@@ -0,0 +1,35 @@
+#ifndef _DIR_SYMBOLS_IMGS
+#define _DIR_SYMBOLS_IMGS
+#include "lvgl_helpers.h"
+#include <stdio.h>
+const lv_img_dsc_t ic_nav_arrow_finish;
+const lv_img_dsc_t ic_nav_arrow_uturn;
+const lv_img_dsc_t ic_nav_roundabout_fallback;
+const lv_img_dsc_t two;
+const lv_img_dsc_t ic_nav_arrow_keep_left;
+const lv_img_dsc_t ic_nav_roundabout_cw3_3;
+const lv_img_dsc_t ic_nav_roundabout_ccw2_2;
+const lv_img_dsc_t ic_nav_arrow_keep_going;
+const lv_img_dsc_t ic_nav_arrow_fork_left;
+const lv_img_dsc_t ic_nav_roundabout_cw1_1;
+const lv_img_dsc_t ic_nav_arrow_turn_left;
+const lv_img_dsc_t ic_nav_roundabout_cw2_2;
+const lv_img_dsc_t ic_nav_roundabout_exit_ccw;
+const lv_img_dsc_t ic_nav_roundabout_ccw3_3;
+const lv_img_dsc_t ic_nav_arrow_turn_right;
+const lv_img_dsc_t ic_nav_arrow_turn_hard_right;
+const lv_img_dsc_t ic_nav_roundabout_ccw2_3;
+const lv_img_dsc_t ic_nav_roundabout_cw1_3;
+const lv_img_dsc_t ic_nav_arrow_turn_hard_left;
+const lv_img_dsc_t ic_nav_roundabout_ccw1_1;
+const lv_img_dsc_t ic_nav_roundabout_exit_cw;
+const lv_img_dsc_t ic_nav_roundabout_ccw1_3;
+const lv_img_dsc_t ic_nav_arrow_keep_right;
+const lv_img_dsc_t ic_nav_arrow_fork_right;
+const lv_img_dsc_t ic_nav_roundabout_cw2_3;
+const lv_img_dsc_t ic_nav_outof_route;
+const lv_img_dsc_t ic_nav_roundabout_ccw1_2;
+const lv_img_dsc_t ic_nav_arrow_goto_start;
+const lv_img_dsc_t ic_nav_arrow_start;
+const lv_img_dsc_t ic_nav_roundabout_cw1_2;
+#endif
