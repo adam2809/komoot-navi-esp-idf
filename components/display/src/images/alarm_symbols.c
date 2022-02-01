fa370acab49882bb87e63e50e8c7d3364ee0c9de
andek <adam.kule@gmail.com>
2022-02-01 16:56:25 +0100

    Removes useless files

--- components/display/src/images/alarm_symbols.c
+++
@@ -1,5 +0,0 @@
-#include "alarm_symbols.h"
-
-
-const lv_img_dsc_t* locked_ptr = &locked;
-const lv_img_dsc_t* unlocked_ptr = &unlocked;
--- components/display/src/images/alarm_symbols.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "lvgl_helpers.h"
-#include <stdio.h>
-
-const lv_img_dsc_t* locked_ptr;
-const lv_img_dsc_t* unlocked_ptr;
-
