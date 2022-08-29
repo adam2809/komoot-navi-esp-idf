95aaf4b200ec85f9c9d7391104b831a792c40dd6
Adam Kulesza <adam.kule@gmail.com>
2022-08-29 17:23:42 +0200

    Adds statechart init to main

--- main/main.c
+++ main/main.c
@@ -35,6 +35,7 @@
 #include "mpu6050.h"
 #include "button.h"
 #include "morse.h"
+#include "Statechart.h"
 
 #define TAG "MAIN"
 #define BUTTONS_BITMASK (PIN_BIT(CONFIG_LEFT_BUTTON_PIN) | PIN_BIT(CONFIG_RIGHT_BUTTON_PIN)) 
@@ -45,6 +46,8 @@ void wakeup();
 TaskHandle_t display_task_handle = NULL;
 QueueHandle_t button_events = NULL;
 morse_input_params_t morse_input_params;
+Statechart statechart;
+
 
 void app_main(){
     esp_err_t ret = nvs_flash_init();
@@ -59,7 +62,9 @@ void app_main(){
     xTaskCreatePinnedToCore(display_task, "display_task", 4096*2, NULL, 0, &display_task_handle, 1);
     
     configure_mpu();
-    wakeup(&button_events,&display_task_handle);
+
+    statechart_init(&statechart);
+    statechart_enter(&statechart);
 }
 
 void gpio_init(){
@@ -74,47 +79,4 @@ void gpio_init(){
     gpio_set_level(GPIO_NUM_4, 0);
     gpio_set_level(GPIO_NUM_25, 1);
     gpio_set_level(CONFIG_BUZZER_PIN, 0);
-
-}
-
-void wakeup(){
-    morse_input_params.buttons_events = &button_events;
-    morse_input_params.display_task_handle = &display_task_handle;
-    switch (esp_sleep_get_wakeup_cause()) {
-        case ESP_SLEEP_WAKEUP_EXT1: {
-            uint64_t wakeup_pin_mask = esp_sleep_get_ext1_wakeup_status();
-            if (wakeup_pin_mask != 0) {
-                int pin = __builtin_ffsll(wakeup_pin_mask) - 1;
-                ESP_LOGI(TAG,"Wake up from GPIO %d", pin);
-                if(pin == CONFIG_LEFT_BUTTON_PIN || pin == CONFIG_RIGHT_BUTTON_PIN){
-                    if (get_lock_state()){
-                        xTaskCreate(&morse_password_input_task, "morse_password_input_task", 4098, (void*) &morse_input_params, 5, NULL);
-                    }else{                        
-                        if(pin == CONFIG_LEFT_BUTTON_PIN){
-                            xTaskCreate(&lock, "lock_task", 4098, NULL, 5, NULL);
-                        }else{    
-                            init_komoot_ble_client(&display_task_handle);
-                        }
-                    }
-                }else if(pin == CONFIG_MPU6050_INTERRUPT_INPUT_PIN && get_lock_state()){
-                    raise_alarm_state();
-                }
-            } else {
-                ESP_LOGW(TAG,"Could not get wakeup pin number");
-            }
-            break;
-        }
-        case ESP_SLEEP_WAKEUP_EXT0: {
-            ESP_LOGI(TAG,"Wake up from EXT0");
-            break;
-        }
-        case ESP_SLEEP_WAKEUP_UNDEFINED:{
-            ESP_LOGI(TAG,"Undefined wakeup reason");
-            init_lock_state();
-            go_to_deep_sleep(false);
-            break;
-        }
-        default:
-            ESP_LOGI(TAG,"Not a deep sleep reset");
-    }
 }
