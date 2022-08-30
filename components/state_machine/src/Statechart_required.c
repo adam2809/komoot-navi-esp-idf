b7788699efb4612d48aa20ec219c5004f18f85b4
Adam Kulesza <adam.kule@gmail.com>
2022-08-30 22:56:51 +0200

    Implements password statechart functions

--- components/state_machine/src/Statechart_required.c
+++ components/state_machine/src/Statechart_required.c
@@ -4,6 +4,11 @@
 #define TAG "STATECHART_REQUIRED"
 
 TaskHandle_t alarm_ringing_task_handle = NULL;
+uint32_t _password = 1;
+
+uint32_t get_password(){
+    return _password;
+}
 
 extern void statechart_disable_alarm( Statechart* handle){
     ESP_LOGI(TAG,"Disabling alarm");
@@ -12,19 +17,40 @@ extern void statechart_disable_alarm( Statechart* handle){
         vTaskDelete(alarm_ringing_task_handle);
     }
 }
+
 extern void statechart_activate_alarm( Statechart* handle){
     ESP_LOGI(TAG,"Activating alarm");
     xTaskCreate(alarm_ringing_task, "alarm_ringing_task", 4098, NULL, 5, alarm_ringing_task_handle);
 }
+
+
 extern void statechart_display_unlocked( Statechart* handle){
     ESP_LOGI(TAG,"Displaying unlocked");
     display_notif(NOTIFY_VALUE_UNLOCK, 1500, handle->display_task_handle);
 }
+
 extern void statechart_display_locked( Statechart* handle){
     ESP_LOGI(TAG,"Displaying locked");
     display_notif(NOTIFY_VALUE_LOCK, 1500, handle->display_task_handle);
 }
-extern void statechart_new_password_input( Statechart* handle, const sc_integer password){}
+
+extern void statechart_new_password_input( Statechart* handle, const sc_integer password){
+    _password = password;
+    xTaskNotify(
+        handle->display_task_handle,
+        NOTIFY_VALUE_MORSE,
+        eSetValueWithOverwrite
+    );
+}
+
 extern sc_boolean statechart_verify_password( Statechart* handle, const sc_integer password){
-    return false;
+    bool res = password == 0b1110010;
+    if(!res){
+        xTaskNotify(
+            handle->display_task_handle,
+            NOTIFY_VALUE_WRONG_PASS,
+            eSetValueWithOverwrite
+        );
+    }
+    return res;
 }
